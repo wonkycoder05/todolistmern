ef3495626f7bb6b06371026ffdae6b05cacd1b02
wonkycoder05 <magdalena.aretha@gmail.com>
2025-05-14 19:05:46 +0700

    sorry it took so long but it works now

--- controllers/todolist.js
+++ controllers/todolist.js
@@ -1,6 +1,6 @@
 import TodolistModel from "../models/todolistModel.js";
 
-
+// create to-do
 export const createTodo = async (req, res) => {
     try {
         const { todo_image, todo_name, todo_desc, todo_status } = req.body
@@ -21,3 +21,53 @@ export const createTodo = async (req, res) => {
         return res.status(500).json({ message: error.message });
     }
 }
+
+// get all to-dos
+export const getAllTodos = async (req, res) => {
+    try {
+        const todos = await TodolistModel.find();
+        res.status(200).json(todos);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+// Update to-do
+export const updateTodo = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { todo_image, todo_name, todo_desc, todo_status } = req.body;
+
+        const updateData = {
+            todo_image,
+            todo_name,
+            todo_desc,
+            todo_status,
+        }
+        const updatedTodo = await TodolistModel.findByIdAndUpdate(id, updateData, { new: true });
+
+        if (!updatedTodo) {
+            return res.status(404).json({ message: "To-do not found." });
+        }
+
+        res.status(200).json({ message: "To-do updated successfully!", updatedTodo });
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+// Delete to-do
+export const deleteTodo = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedTodo = await TodolistModel.findByIdAndDelete(id);
+
+        if (!deletedTodo) {
+            return res.status(404).json({ message: "To-do not found." });
+        }
+
+        res.status(200).json({ message: "To-do deleted successfully!" });
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
