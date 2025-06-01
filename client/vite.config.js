9ae60ae390b6956fa788737344e69ad90c3c7dbf
wonkycoder05 <magdalena.aretha@gmail.com>
2025-06-01 23:41:28 +0700

    deploy cicd pipeline
    Signed-off-by: wonkycoder05 <magdalena.aretha@gmail.com>

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,12 +3,17 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
   return {
     server: {
       proxy: {
         "/service": {
-          target: "http://localhost:5000",
+          target: mode === "development"
+            ? "http://localhost:5000"
+            : "e2425-wads-l4bcg1-magdalena.csbihub.id",
+          changeOrigin: true,
+          secure: mode !== "development",
+          ws: true,
         },
       },
     },
